10538a23fb67933abe5b492eb4ae771bd646f8d6
Daniel Toby <danieltoby@Daniels-MBP.cable.rcn.com>
2022-04-16 11:01:20 -0400

    Style UI.

--- Asciiboard/asciiboard.cpp
+++ Asciiboard/asciiboard.cpp
@@ -84,8 +84,8 @@ public:
         auto square = static_cast<int>(controls.squareWeight * 100);
         auto triangle = static_cast<int>(controls.triangleWeight * 100);
         auto sineSlider = Slider("Sine:", &sine, 0, 100, 1);
-        auto squareSlider = Slider("Square:", &square, 0, 100, 1);
-        auto triangleSlider = Slider("Triangle:", &triangle, 0, 100, 1);
+        auto squareSlider = Slider(" Square:", &square, 0, 100, 1);
+        auto triangleSlider = Slider(" Triangle:", &triangle, 0, 100, 1);
 
         auto oscillatorControlsContainer = Container::Horizontal({
             sineSlider,
@@ -96,32 +96,27 @@ public:
         auto oscillatorControls = Renderer(oscillatorControlsContainer,
                                            [&sineSlider, &squareSlider, &triangleSlider]() {
                                                return hbox({sineSlider->Render(),
-                                                            filler(),
                                                             squareSlider->Render(),
-                                                            filler(),
                                                             triangleSlider->Render()});
                                            });
 
         // Oscilloscope
         auto scaleFactor = 0.5;
-        auto graphHeight = 60;
+        auto graphHeight = 40;
         auto oscilloscope = Renderer([&] {
-            auto width = 200;
+            auto width = static_cast<int>(_lastOutputBuffer.size());
             auto c = Canvas(width, graphHeight);
             for (auto i = 0; i < width - 1; ++i) {
-                if (i < static_cast<int>(_lastOutputBuffer.size())) {
-                    c.DrawPointLine(i,
-                                    (_lastOutputBuffer[i] * graphHeight * scaleFactor) + (graphHeight / 2),
-                                    i + 1,
-                                    (_lastOutputBuffer[i + 1] * graphHeight * scaleFactor) + (graphHeight / 2));
-                } else {
-                    c.DrawPointLine(i, graphHeight / 2, i + 1, graphHeight / 2);
-                }
+                c.DrawPointLine(i,
+                                (_lastOutputBuffer[i] * graphHeight * scaleFactor) + (graphHeight / 2),
+                                i + 1,
+                                (_lastOutputBuffer[i + 1] * graphHeight * scaleFactor) + (graphHeight / 2),
+                                Color::Purple);
             }
 
             return vbox({
                 text(fmt::format("Frequency [Mhz] x ({})", scaleFactor)) | hcenter,
-                canvas(std::move(c))
+                canvas(std::move(c)) | hcenter
             });
         });
 
@@ -131,9 +126,9 @@ public:
 
         auto oscillators = Renderer(oscillatorsContainer, [&] {
             return vbox({
-                oscilloscope->Render() | size(HEIGHT, LESS_THAN, graphHeight),
+                oscilloscope->Render() | flex,
                 oscillatorControls->Render(),
-            }) | size(WIDTH, EQUAL, 66) | borderRounded | hcenter;
+            }) | borderRounded | hcenter | color(Color::BlueLight);
         });
 
         // =========== Envelope tab ===========
@@ -143,10 +138,10 @@ public:
         auto decay = static_cast<int>(controls.decay * 100);
         auto sustain = static_cast<int>(controls.sustain * 100);
         auto release = static_cast<int>(controls.release * 100);
-        auto attackSlider = Slider("Attack:", &attack, 0, 100, 1);
-        auto decaySlider = Slider("Decay:", &decay, 0, 100, 1);
-        auto sustainSlider = Slider("Sustain:", &sustain, 0, 100, 1);
-        auto releaseSlider = Slider("Release:", &release, 0, 100, 1);
+        auto attackSlider = Slider("Attack:", &attack, 1, 100, 1);
+        auto decaySlider = Slider(" Decay:", &decay, 1, 100, 1);
+        auto sustainSlider = Slider(" Sustain:", &sustain, 0, 100, 1);
+        auto releaseSlider = Slider(" Release:", &release, 1, 100, 1);
 
         auto envelopeControlsContainer = Container::Horizontal({
             attackSlider,
@@ -159,18 +154,16 @@ public:
                                          [&attackSlider, &decaySlider, &sustainSlider, &releaseSlider] () {
                                              return hbox({
                                                  attackSlider->Render(),
-                                                 filler(),
                                                  decaySlider->Render(),
-                                                 filler(),
                                                  sustainSlider->Render(),
-                                                 filler(),
                                                  releaseSlider->Render()
                                              });
                                          });
 
         auto envelopeGraph = Renderer([&] {
-            auto width = 200;
+            auto width = 80;
             auto c = Canvas(width, graphHeight);
+            auto effectiveHeight = graphHeight - 5;
              /*
              *           / \
              *         /    \
@@ -191,23 +184,30 @@ public:
             auto x2 = ((static_cast<double>(attack) + decay) / totalTime) * remainingWidth;
             auto x3 = x2 + sustainWidth;
 
-            auto sustainHeight = graphHeight - (graphHeight * (static_cast<double>(sustain) / 100));
+            auto sustainHeight = effectiveHeight - (effectiveHeight * (static_cast<double>(sustain) / 100));
 
-            c.DrawPointLine(0, graphHeight, x1, 0);
-            c.DrawPointLine(x1, 0, x2, sustainHeight);
-            c.DrawPointLine(x2, sustainHeight, x3, sustainHeight);
-            c.DrawPointLine(x3, sustainHeight, width, graphHeight);
+            c.DrawPointLine(0, effectiveHeight, x1, 0, Color::Cyan);
+            c.DrawPointLine(x1, 0, x2, sustainHeight, Color::BlueLight);
+            c.DrawPointLine(x2, sustainHeight, x3, sustainHeight, Color::Purple);
+            c.DrawPointLine(x3, sustainHeight, width, effectiveHeight, Color::Red);
 
-            return canvas(std::move(c));
+            return vbox({
+                text(fmt::format("Envelope (ms)", scaleFactor)) | hcenter,
+                canvas(std::move(c)) | hcenter
+            });
         });
 
         auto envelopeContainer = Container::Vertical({ envelopeControls });
 
         auto envelope = Renderer(envelopeContainer, [&] {
             return vbox({
-                envelopeGraph->Render() | size(HEIGHT, LESS_THAN, graphHeight),
+                         hbox({
+                           filler(),
+                           envelopeGraph->Render(),
+                           filler()
+                       }),
                 envelopeControls->Render()
-            }) | size(WIDTH, EQUAL, 66) | borderRounded | hcenter;
+            }) | borderRounded | color(Color::BlueLight);
         });
 
         auto tab_index = 0;
@@ -240,10 +240,14 @@ public:
         };
 
         auto pianoRoll = Renderer([&activeNotes, &keyboard]() {
-            return vbox({
-                         graph(std::ref(activeNotes)) | size(HEIGHT, EQUAL, 2) | color(Color::GreenLight),
-                         graph(std::ref(keyboard)) | size(HEIGHT, EQUAL, 2)
-                        }) | size(WIDTH, EQUAL, 66) | borderRounded | hcenter;
+            return hbox({
+                        filler(),
+                        vbox({
+                            graph(std::ref(activeNotes)) | size(HEIGHT, EQUAL, 2) | color(Color::GreenLight),
+                           graph(std::ref(keyboard)) | size(HEIGHT, EQUAL, 2) | color(Color::Default)
+                        }) | size(WIDTH, EQUAL, 66),
+                        filler()
+                   }) | borderRounded | color(Color::RedLight);
         });
 
         auto mainContents = Container::Vertical({
