8ff17f017aabc67d2ba941dbcc7eda19299e2fc3
Daniel Toby <daniel.e.toby@gmail.com>
2026-01-20 17:40:01 -0500

    Remove redundant operations in Oscillator.

--- synth/src/synth/oscillator.hpp
+++ synth/src/synth/oscillator.hpp
@@ -11,24 +11,24 @@ namespace synth {
 class Oscillator {
 public:
     Oscillator(double frequency, double sampleRate) :
-        _frequency{frequency},
+        _increment{toIncrement(frequency, sampleRate)},
         _sampleRate{sampleRate},
         _currentIndex{0} {}
 
     Oscillator(const Oscillator& other) :
-        _frequency{other._frequency},
+        _increment{other._increment},
         _sampleRate{other._sampleRate},
         _currentIndex{0} {
     }
 
     template <std::size_t NumWaveTables>
     float nextSample(const WeightedWaveTables<NumWaveTables>& weightedWaveTables) {
-        // Linear interpolation improves the signal approximation accuracy at discrete index.
+        // Linear interpolation improves the signal approximation accuracy at discrete indices.
         auto indexBelow = static_cast<int>(std::floor(_currentIndex));
         auto indexAbove = (indexBelow + 1) % WAVETABLE_LENGTH;
         auto fractionAbove = _currentIndex - indexBelow;
-        auto fractionBelow = 1.0 - fractionAbove;
-        _currentIndex = std::fmod((_currentIndex + WAVETABLE_LENGTH * _frequency / _sampleRate), WAVETABLE_LENGTH);
+        auto fractionBelow = 1 - fractionAbove;
+        _currentIndex = std::fmod(_currentIndex + _increment, WAVETABLE_LENGTH);
 
         auto nextSample = 0.0f;
         for (auto i = 0; i < NumWaveTables; i++) {
@@ -40,13 +40,17 @@ public:
         return nextSample;
     }
 
-    void setFrequency(float frequency) {
-        _frequency = frequency;
+    void setFrequency(double frequency) {
+        _increment = toIncrement(frequency, _sampleRate);
     }
 
 private:
-    double _frequency;
+    [[nodiscard]] static double toIncrement(double frequency, double sampleRate) {
+        return WAVETABLE_LENGTH * frequency / sampleRate;
+    }
+
     double _sampleRate;
+    double _increment;
     double _currentIndex;
 };
 }
