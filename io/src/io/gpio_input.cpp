09c265055cc7e5e2b07339bc76511788d331e84a
Daniel Toby <daniel.e.toby@gmail.com>
2026-02-28 00:47:38 -0500

    Added GPIOInput class.

--- io/src/io/gpio_input.cpp
+++ io/src/io/gpio_input.cpp
@@ -10,19 +10,132 @@
 
 #endif
 
+#include <atomic>
+#include <thread>
+
+#include "common/exception.hpp"
+#include "common/log.hpp"
+
 namespace io {
+
+#ifdef ENABLE_GPIO_CONTROL
+
+namespace {
+
+struct LibGPIOManager {
+    gpiod::chip chip;
+    gpiod::line_request lineRequest;
+};
+
+//! Transforms the public facing config into a libgpiod config. Throws on failure!
+[[nodiscard]] LibGPIOManager makeGPIOManager(const HardwareConfiguration& config) noexcept(false) {
+    gpiod::chip chip(config.chipName.c_str());
+    auto request = chip.prepare_request();
+
+    gpiod::line_settings settings;
+    settings.set_direction(gpiod::line::direction::INPUT);
+    settings.set_edge_detection(gpiod::line::edge::BOTH);
+    settings.set_bias(gpiod::line::bias::PULL_UP);
+    settings.set_debounce_period(std::chrono::milliseconds(3));
+
+    GPIOState reservedPins;
+    for (std::size_t pin = 1; pin < GPIOState::maxPin; ++pin) {
+        for (const auto& component : config.components) {
+            if (reservedPins.isOn(pin)) {
+                throw common::MicrotoneException("Multiple components connected to the same pin.");
+            }
+
+            if (component->isPinReserved(pin)) {
+                request.add_line_settings(pin, settings);
+                reservedPins.setPin(pin, true);
+            }
+        }
+    }
+
+    request.set_consumer(config.consumerName);
+    return {std::move(chip), request.do_request()};
+}
+
+[[nodiscard]] GPIOState getInitialState(LibGPIOManager& manager, const HardwareConfiguration& config) {
+    GPIOState state;
+    for (std::size_t pin = 1; pin < GPIOState::maxPin; ++pin) {
+        for (const auto& component : config.components) {
+            if (component->isPinReserved(pin)) {
+                const bool isOn = manager.lineRequest.get_value(pin) == gpiod::line::value::ACTIVE;
+                state.setPin(pin, isOn);
+            }
+        }
+    }
+    return state;
+}
+
+}
 
 class GPIOInput::impl {
 public:
-    impl() {
+    explicit impl(const HardwareConfiguration& hardwareConfig) :
+        _hardwareConfig(hardwareConfig),
+        _gpioManager(makeGPIOManager(_hardwareConfig)),
+        _gpioState(getInitialState(_gpioManager, _hardwareConfig)) {
+    }
+
+    void start() {
+        _running = true;
+        _thread = std::thread(&impl::processLoop, this);
+    }
+
+    void stop() {
+        _running = false;
+        if (_thread.joinable()) {
+            _thread.join();
+        }
     }
 
     ~impl() {
+        stop();
     }
+
+private:
+    void processLoop() {
+        while (_running) {
+            for (const auto& edgeEvent : _eventBuffer) {
+                const auto pin = edgeEvent.line_offset();
+                for (const auto& component : _hardwareConfig.components) {
+                    if (component->isPinReserved(pin)) {
+                        const bool isOn = _gpioManager.lineRequest.get_value(pin) == gpiod::line::value::ACTIVE;
+                        _gpioState.setPin(pin, isOn);
+                        component->update(_gpioState);
+                    }
+                }
+            }
+        }
+    }
+
+    HardwareConfiguration _hardwareConfig;
+    LibGPIOManager _gpioManager;
+    GPIOState _gpioState;
+    gpiod::edge_event_buffer _eventBuffer;
+
+    std::atomic<bool> _running{false};
+    std::thread _thread;
 };
 
-GPIOInput::GPIOInput() :
-    _impl{std::make_unique<impl>()} {
+#else
+
+
+class GPIOInput::impl {
+public:
+    explicit impl(const HardwareConfiguration& hardwareConfig) {}
+    static void start() {
+        M_INFO("GPIO input is disabled.");
+    }
+    static void stop() {}
+};
+
+#endif
+
+GPIOInput::GPIOInput(const HardwareConfiguration& config) :
+    _impl{std::make_unique<impl>(config)} {
 }
 
 GPIOInput::GPIOInput(GPIOInput&& other) noexcept :
--- io/src/io/gpio_input.hpp
+++ io/src/io/gpio_input.hpp
@@ -1,13 +1,134 @@
 #pragma once
 
+#include <functional>
 #include <memory>
+#include <string>
 
 namespace io {
+
+//! This library supports a 1-indexed 40 pin GPIO.
+class GPIOState {
+public:
+    static constexpr std::size_t maxPin = 41;
+
+    GPIOState() = default;
+    GPIOState(const GPIOState&) = default;
+
+    [[nodiscard]] bool isOn(std::size_t pin) const {
+        return _zeroIndexedState[pin - 1];
+    }
+
+    void setPin(std::size_t pin, bool value) {
+        _zeroIndexedState[pin - 1] = value;
+    }
+
+private:
+    std::array<bool, maxPin -1> _zeroIndexedState;
+};
+
+//! Occupies some compile-time known number of pins.
+//! If any of those pins are affected when events are being processed, "processEvents" is called with the latest state.
+class I_GPIOComponent {
+public:
+    virtual ~I_GPIOComponent() = default;
+
+    //! Pins are 1-indexed.
+    [[nodiscard]] virtual bool isPinReserved(std::size_t pin) const = 0;
+
+    //! This function is called when a reserved pin is changed.
+    virtual void update(const GPIOState& state) = 0;
+};
+
+using OnEventFn = std::function<void()>;
+
+struct PushButtonConfig {
+    std::size_t pin;
+    OnEventFn onPressed;
+    OnEventFn onReleased;
+};
+
+class PushButton : public I_GPIOComponent {
+public:
+    PushButton(const GPIOState& initialState, PushButtonConfig config) : _previousState(initialState), _config(std::move(config)) {}
+
+    [[nodiscard]] bool isPinReserved(std::size_t pin) const override {
+        return pin == _config.pin;
+    }
+
+    void update(const GPIOState& state) override {
+        if (_previousState.isOn(_config.pin) && !state.isOn(_config.pin)) {
+            std::invoke(_config.onPressed);
+        } else if (!_previousState.isOn(_config.pin) && state.isOn(_config.pin)) {
+            std::invoke(_config.onReleased);
+        }
+        _previousState = state;
+    }
+
+private:
+    GPIOState _previousState;
+    PushButtonConfig _config;
+};
+
+struct RotaryEncoderConfig {
+    std::size_t CLK;
+    std::size_t DT;
+    OnEventFn onCWTurn;
+    OnEventFn onCCWTurn;
+};
+
+class RotaryEncoder : public I_GPIOComponent {
+public:
+    RotaryEncoder(const GPIOState& initialState, const RotaryEncoderConfig& config) :
+        _previousState(packState(config, initialState)),
+        _config(config) {}
+
+    [[nodiscard]] bool isPinReserved(std::size_t pin) const override {
+        return pin == _config.CLK || pin == _config.DT;
+    }
+
+    //! Based on a quadrature state machine.
+    void update(const GPIOState& state) override {
+        // No rotation = 0, CW = +1, CCW = -1;
+        constexpr std::array<int8_t, 16> table = {
+            0,  +1, -1,  0,
+           -1,   0,  0, +1,
+           +1,   0,  0, -1,
+            0,  -1, +1,  0
+        };
+
+        auto currentState = packState(_config, state);
+
+        auto index = (_previousState << 2) | currentState;
+        auto value = table[index];
+        if (value == 1) {
+            std::invoke(_config.onCWTurn);
+        } else if (value == -1) {
+            std::invoke(_config.onCCWTurn);
+        }
+
+        _previousState = currentState;
+    }
+
+private:
+    [[nodiscard]] static int packState(const RotaryEncoderConfig& config, const GPIOState& state) {
+        return static_cast<int>(state.isOn(config.CLK)) << 1 | static_cast<int>(config.DT);
+    }
+
+    int _previousState;
+    RotaryEncoderConfig _config;
+};
+
+//! The hardware API supported by this library.
+struct HardwareConfiguration {
+    std::string chipName;
+    std::string consumerName;
+    std::vector<std::shared_ptr<I_GPIOComponent>> components;
+};
 
 //! This is the portaudio wrapper.
 class GPIOInput {
 public:
-    GPIOInput();
+    explicit GPIOInput(const HardwareConfiguration& config);
     GPIOInput(const GPIOInput&) = delete;
     GPIOInput& operator=(const GPIOInput&) = delete;
     GPIOInput(GPIOInput&&) noexcept;
